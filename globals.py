803ddb6cc8fee67e461964a28e3b0fe9c5944ac1
mapa21 <63611280+mapa21@users.noreply.github.com>
2024-05-22 14:32:21 +0200

    #13 Add: compute t, the upper bound on the number of failures tolerated depending on n

--- globals.py
+++ globals.py
@@ -2,7 +2,11 @@
 import numpy as np
 
 n = 4
-t = 0
+# t denotes an upper bound on the number of failures tolerated
+t = int(np.floor(n/3))
+if n%3 == 0:
+    t -= 1
+
 first_to_decide = None
 
 MAX_ALIVE_PROCESSES = n-t
