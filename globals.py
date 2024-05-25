d0d11ef56906632c60af71dbfb4b9dcf2b2a2c74
mapa21 <63611280+mapa21@users.noreply.github.com>
2024-05-25 10:54:18 +0200

    #17 Add: coin and leader lock as global vars to be used also by adversary changing messages receivers

--- globals.py
+++ globals.py
@@ -1,5 +1,6 @@
 # From FailStop
 import numpy as np
+from threading import Lock
 
 n = 4
 # t denotes an upper bound on the number of failures tolerated
@@ -15,3 +16,10 @@ class Message:
     def __init__(self, process_id, receivers) -> None:
         self.sender = process_id
         self.receivers = receivers
+
+# From QuantumCoinFlip
+coin_lock = Lock()
+leader_lock = Lock()
+
+# From Adversary
+num_faults = 0
